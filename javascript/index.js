05d62fbf51e8035c98c6c6e79a75aaf364f1d40c
aelvanna <lmarie.fernandez@gmail.com>
2023-04-23 19:24:07 +1000

    updated website design

--- javascript/index.js
+++ javascript/index.js
@@ -23,6 +23,18 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  // Manila
+  let manilaElement = document.querySelector("#manila");
+  if (manilaElement) {
+    let manilaDateElement = manilaElement.querySelector(".date");
+    let manilaTimeElement = manilaElement.querySelector(".time");
+    let manilaTime = moment().tz("Asia/Manila");
+
+    manilaDateElement.innerHTML = manilaTime.format("MMMM Do YYYY");
+    manilaTimeElement.innerHTML = manilaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
@@ -39,7 +51,7 @@ function updateCity(event) {
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    <div class="time">${cityTime.format("h:mm:ss ")}<small>${cityTime.format(
     "A"
   )}</small></div>
     </div>
